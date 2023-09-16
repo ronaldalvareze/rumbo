4bc3776071fa4ffece642672b33fa0e6e4b4284d
ocaso1323 <ap3717402@gmail.com>
2023-09-15 20:30:21 -0500

    terminacion de req.params en user y despacho

--- backend/src/controllers/despachos.controller.js
+++ backend/src/controllers/despachos.controller.js
@@ -63,4 +63,52 @@ export const createDespachos = async (req, res) => {
 
 export const updateDespachos = (req, res) => res.send('Actualizando Despachos');
 
+export const updateDespacho = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      hora_salida,
+      ruta,
+      origen,
+      destino,
+      fk_id_conductor,
+      fk_id_despachador,
+      fecha_hora_de_creacion,
+    } = req.body;
+
+    const [result] = await pool.query(
+      "UPDATE despachos SET hora_salida = IFNULL(?, hora_salida), ruta = IFNULL(?, ruta), origen = IFNULL(?, origen), destino = IFNULL(?, destino), fk_id_conductor = IFNULL(?, fk_id_conductor), fk_id_despachador = IFNULL(?, fk_id_despachador), fecha_hora_de_creacion = IFNULL(?, fecha_hora_de_creacion) WHERE id_despacho = ?",
+      [hora_salida, ruta, origen, destino, fk_id_conductor, fk_id_despachador, fecha_hora_de_creacion, id]
+    );
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: 'Despacho not found' });
+    }
+
+    res.status(200).json({ message: 'Despacho updated successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
 export const deleteDespachos = (req, res) => res.send('Borrando Despachos');
+
+export const deleteDespacho = async (req, res) => {
+  try {
+    const [result] = await pool.query("DELETE FROM despachos WHERE id_despacho = ?", [
+      req.params.id,
+    ]);
+
+    if (result.affectedRows <= 0) {
+      return res.status(404).json({
+        message: 'Despacho no encontrado',
+      });
+    }
+
+    res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Algo va mal',
+    });
+  }
+};
