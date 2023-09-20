fc293e4cbf969a2ce9810b934a5fb7e1f715bef8
ronaldalvareze <ronaldalvarez172@gmail.com>
2023-09-19 20:25:38 -0500

    correciones de endpoints

--- backend/src/controllers/despachos.controller.js
+++ backend/src/controllers/despachos.controller.js
@@ -1,15 +1,13 @@
-import { json } from 'express'
 import { pool } from '../db.js'
 
 
 export const getDespachos = async (req, res) => {
   try {
-    //throw new Error('mi error')
     const [rows] = await pool.query('SELECT * FROM despachos');
     res.json(rows);
   } catch (error) {
-    res.status(500).json({ 
-      message: 'algo va mal', 
+    return res.status(500).json({ 
+      message: 'algo va mal X)', 
     });
   }
 };
@@ -27,7 +25,7 @@ export const getDespacho = async (req, res) => {
     res.json(rows[0]);
   } catch (error) {
      return res.status(500).json({ 
-      message: 'algo va mal', 
+      message: 'algo va mal X)', 
     });
   }
 };
@@ -60,7 +58,7 @@ export const createDespachos = async (req, res) => {
     });
   } catch (error) {
     return  res.status(500).json({
-       message: 'algo va mal',
+       message: 'algo va mal X)',
        });
   }
 };
@@ -85,16 +83,17 @@ export const updateDespacho = async (req, res) => {
       [hora_salida, ruta, origen, destino, fk_id_conductor, fk_id_despachador, fecha_hora_de_creacion, id]
     );
 
-    if (result.affectedRows === 0) {
-      return res.status(404).json({ message: 'Despacho not found' });
+    if (result.affectedRows <= 0) {
+      return res.status(404).json({ message: 'Despacho no encontrado' });
     }
 
     res.status(200).json({ message: 'Despacho updated successfully' });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'algo va mal' });
+    return res.status(500).json({
+      message: 'algo va mal x)' 
+    })
   }
-};
+}
 export const deleteDespachos = (req, res) => res.send('Borrando Despachos');
 
 export const deleteDespacho = async (req, res) => {
@@ -112,7 +111,7 @@ export const deleteDespacho = async (req, res) => {
     res.sendStatus(204);
   } catch (error) {
     return res.status(500).json({
-      message: 'Algo va mal',
+      message: 'Algo va mal X)',
     });
   }
 };
